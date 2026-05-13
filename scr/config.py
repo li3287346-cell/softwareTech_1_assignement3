e3ac154b6b4fa1a99925dc6a1f14cf968f3aa316
li3287346-cell <li3287346@gmail.com>
2026-05-14 09:58:24 +1000

    Create config.py

---
+++ scr/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+
+DATASET_PATH = BASE_DIR / "data" / "raw" / "stream_macroinvertebrates"
+OUTPUT_FOLDER = BASE_DIR / "outputs" / "eda"
+
+SUPPORTED_FORMATS = (".jpg", ".jpeg", ".png", ".bmp")
+IMAGE_SIZE = (128, 128)
