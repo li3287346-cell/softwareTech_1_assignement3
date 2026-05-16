ce678e5f2f6f22926cb89669dade2a9a8f64aae6
sidra934 <u3317210@uni.canberra.edu.au>
2026-05-16 16:49:41 +1000

    Update main.py

--- scr/main.py
+++ scr/main.py
@@ -2,10 +2,16 @@ import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.config import DATASET_PATH, OUTPUT_FOLDER
 from src.services.eda_service import EDAService
+from src.app import MacroApp
+
 
 def main():
     eda = EDAService(DATASET_PATH, OUTPUT_FOLDER)
     eda.run_all()
+    
+    # Launch Stage 3 GUI
+    app = MacroApp()
+    app.mainloop()
 
 if __name__ == "__main__":
     main()
