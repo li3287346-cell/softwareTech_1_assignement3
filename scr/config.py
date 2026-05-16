03f7a29d88c2ee13c9f6bd821dae1d166d9e7e75
sidra934 <u3317210@uni.canberra.edu.au>
2026-05-16 16:48:27 +1000

    Update config.py

--- scr/config.py
+++ scr/config.py
@@ -4,6 +4,6 @@ BASE_DIR = Path(__file__).parent.parent
 
 DATASET_PATH = BASE_DIR / "data" / "raw" / "stream_macroinvertebrates"
 OUTPUT_FOLDER = BASE_DIR / "outputs" / "eda"
-
+EDA_OUTPUT_DIR = OUTPUT_FOLDER 
 SUPPORTED_FORMATS = (".jpg", ".jpeg", ".png", ".bmp")
 IMAGE_SIZE = (128, 128)
